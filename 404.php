6bd781a5109f582c8704dc5214f0f6c8585405c5
Anne-France Lambert <a.f.l@hotmail.fr>
2018-04-30 12:44:13 +0200

    wordpress about et page 404

--- 404.php
+++ 404.php
@@ -1,17 +1,18 @@
-
 <?php
 
     get_header();
     
 ?>
-<div class="errorPage404">
-    <h1>page not found</h1>
-    <p>The page you requested was not found.
-    <br/>The link you used may be broken or you may have entered the URL incorrectly.</p>
+    <div class="container-fluid">
+        <div class="errorPage404">
+            <h1>Page not found</h1>
+            <p>The page you requested was not found.
+                <br/>The link you used may be broken or you may have entered the URL incorrectly.</p>
 
-    <a class="buttonGLO" href="index.php">Back homepage</a>
-</div>
+            <a class="buttonGLO" href="index.php">Back homepage</a>
+        </div>
+    </div>
 
-<?php
+    <?php
     get_footer();
 ?>
