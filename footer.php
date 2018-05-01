a2ab860bceff52bb87c42f55a75438f55710ec87
Spootnika <celia.lflr@gmail.com>
2018-05-01 21:18:13 +0200

    Pages terms, contact, nouveau fichier sql à importer dans la base de données

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 
 <?php wp_footer(); ?>
 <footer>
-    <span class="footerText"><a href="terms-conditions.php">terms & conditions</a></span>
+    <span class="footerText"><a href="wordpress/terms-and-conditions">terms & conditions</a></span>
     <span class="sep"> | </span>
     <span class="footerText"><a href="http://www.nodevo.com/" target="_blank">powered by novevo</a></span>
 </footer>
--- page-terms.php
+++ page-terms.php
@@ -7,7 +7,9 @@ get_header();
 if(have_posts()):
     while(have_posts()): 
         the_post();
+        echo '<h1>';
         the_title();
+        echo '</h1>';
         the_content();
     endwhile;
 endif;
